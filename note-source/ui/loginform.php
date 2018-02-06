c75f2787ca9bf8c3207587e9bebf572c0078dea5
Bram Korsten <exentory@gmail.com>
2018-02-06 13:25:42 +0100

    Revert "Update to login screen design for next major release"
    This reverts commit 6eacbbd2859c7683b9b6fc97287e389b8725fcc5.

--- note-source/ui/loginform.php
+++ note-source/ui/loginform.php
@@ -17,20 +17,27 @@
       <h2 class="hello">Hello!</h2>
     </div>
     <div class="login_inner_wrap">
-      <h3>Please login to continue.</h3>
-      <form class="inner_login_form" action="redirect.php" method="POST">
-        <input class="logininput" autocomplete="new-password" type="text" name="user" value="" placeholder="Username" size="20">
-        <br>
-        <input class="logininput" autocomplete="new-password" type="password" placeholder="Password" name="pass" value="">
-        <br>
-        <input class="submitbutton loginbutton" type="submit" name="submit" value="Login">
-      </form>
+      <h3>Welcome to Note</h3>
+      <p>Please login to continue</p>
   </div>
   </div>
-
+  <form class="loginform" action="redirect.php" method="POST">
+    <p class="label">
+      Username
+    </p>
+    <input class="logininput" type="text" name="user" value="" size="20">
+    <br>
+    <p class="label">
+      Password
+    </p>
+    <input class="logininput" type="password" name="pass" value="">
+    <br>
+    <input class="submitbutton loginbutton" type="submit" name="submit" value="Login">
+  </form>
   <div class="note"></div>
   <p class="powered">
-    <i><?php echo($version); ?></i>
+    <i>Powered by Note - <?php echo($version); ?><br>
+      By Bram Korsten</i>
   </p>
   <audio id="audiomelone">
   <source src="video/watermelone.ogg" type="audio/ogg">
